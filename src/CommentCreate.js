584b31aab42ca1a5cb4a68964317cc447158505b
Iron.Snowflake <jadrubio@gmail.com>
2021-08-06 13:19:23 -0400

    Post List and Comment Create components

--- src/CommentCreate.js
+++ src/CommentCreate.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-export default() => {
-  const [title, setTitle] = useState('');
+export default({ postId }) => {
+  const [content, setContent] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    await axios.post('http://localhost:4000/posts',{
-      title
+    await axios.post(`http://localhost:4040/posts/${postId}/comments`,{
+      content
     });
-
-    setTitle('');
+    setContent('');
   };
 
-  return(
+  return (
     <div>
       <form onSubmit={(e) => handleSubmit(e)}>
         <div className="form-group">
-          <label>Title</label>
-          <input value={title} onChange={e => setTitle(e.target.value)} className="form-control" />  
+          <label>New Comment</label>
+          <input
+            className="form-control"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
         </div>
         <button
           className="btn btn-primary"
